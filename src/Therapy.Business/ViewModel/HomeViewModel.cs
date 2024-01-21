704437bf531b186669cb3d598912f9c821873842
TSpg1012 <bd8g57ev2@code.edu.az>
2024-01-21 13:22:14 +0400

    Main part finished

---
+++ src/Therapy.Business/ViewModel/HomeViewModel.cs
@@ -0,0 +1,8 @@
+﻿using Therapy.Core.Models;
+
+namespace Therapy.Business.ViewModel;
+
+public class HomeViewModel
+{
+    public List<Therapist> Therapist { get; set; }
+}
--- src/Therapy.UI/Controllers/HomeController.cs
+++ src/Therapy.UI/Controllers/HomeController.cs
@@ -1,16 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using Therapy.Business.ViewModel;
 using Therapy.Core.Models;
+using Therapy.Data.DAL;
 
 namespace Therapy.UI.Controllers
 {
     public class HomeController : Controller
     {
-        
+        private readonly AppDbContext _context;
+
+        public HomeController(AppDbContext context)
+        {
+            _context = context;
+        }
 
         public IActionResult Index()
         {
-            return View();
+            HomeViewModel vm = new HomeViewModel()
+            {
+                Therapist = _context.Therapists.ToList(),
+            };
+            return View(vm);
         }
 
         
